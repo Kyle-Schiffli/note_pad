0ae9eba604364692fb6a5448c132ecdcc55a824f
Kyle-Schiffli <Kyle.Schiffli@gmail.com>
2019-12-17 16:09:05 -0500

    Add files via upload

---
+++ Words_DB/functions.rb
@@ -0,0 +1,166 @@
+class Database
+  
+  @words = PG.connect(dbname: "words_app")
+  
+  def self.query(sql)
+    @words.exec(sql)
+  end
+  
+  def self.execute(sql, input)
+    
+    @words.exec_params(sql, [input])  
+  end
+  
+  def self.delete(sql)
+    @words.exec(sql)
+  end
+  
+  def self.first_entry
+    @words.exec("SELECT * FROM words LIMIT 1;").values
+  end
+  
+  #find a word in any table, defaults to main words table
+  def self.word_search(word, table = "words")
+    
+    sql = "SELECT * FROM #{table} WHERE word = $1"
+    
+    @result = @words.exec_params(sql, [word]).values
+      if @result.flatten.empty?
+        return false
+      else
+        @result
+      end
+  
+  end  
+  
+  def self.add_line(user_id, line, syllables, syllable_count, end_rhyme)
+    sql = "INSERT INTO all_lines 
+          (user_id, line, syllables, syllable_count, end_rhyme)
+          VALUES ($1, $2, $3, $4, $5)"
+          
+      @words.exec_params(sql, 
+      [user_id, line, syllables, syllable_count, end_rhyme])  
+        
+  end
+  
+  def self.save_all()
+    sql = "UPDATE all_lines SET saved = 'y' WHERE saved = 'n'"
+          
+    @words.exec(sql)
+  end
+  
+  def self.find_all_rhymes(rhyme_group)
+    sql = "SELECT * FROM words WHERE rhyme = $1"
+    @result = @words.exec_params(sql, [rhyme_group]).column_values(1)
+    
+    @result
+  end
+  
+    def self.find_all_syllables(count)
+    sql = "SELECT * FROM words WHERE syllable = $1"
+    @result = @words.exec_params(sql, [count]).column_values(1)
+    
+    @result
+  end
+  
+  def self.add_to_modifier(word)
+    sql = "INSERT INTO modifier (word)
+          VALUES ($1)"
+    @words.exec_params(sql, [word])  
+  end
+  
+  def self.delete_modifier(word)
+    sql = "DELETE FROM modifier WHERE word = $1"
+    @words.exec_params(sql, [word])  
+  end
+  
+  def self.add_word(word, rhyme = 0, syllable = 0)
+    sql = "INSERT INTO words (word, rhyme, syllable)
+          VALUES ($1, $2, $3)"
+    @words.exec_params(sql, [word, rhyme, syllable]) 
+  end
+  
+  def self.modify_word(word, rhyme = 0, syllable = 0)
+    sql = "UPDATE words SET rhyme = $1, syllable = $2
+          WHERE word = $3"
+    @words.exec_params(sql, [rhyme, syllable, word]) 
+  end
+  
+  def self.note_pad_search(word)
+    sql = "UPDATE current_session SET word_search = $1 WHERE id = 1"
+    @words.exec_params(sql, [word])
+  end
+  
+end
+
+helpers do
+#used for notepad
+  def analyze(line, hash)
+    line_array = line.split(" ")
+    result_array = line_array.map do |word|
+      lower_case = word.downcase
+      if hash[lower_case]
+        (hash[lower_case])[0][3]
+      else
+        "?"
+      end
+    end
+    result_array
+  end
+  
+  #uses result from analyze method
+  def syllable_count(result_array)
+    array = result_array.map(&:to_i)
+    count = 0
+    array.each {|element| count += 1 if element == 0}
+    array.inject(:+) + count
+  end
+  
+  def analyzer_breakdown(input)
+    filtered = input.delete(",.!?\":;()")
+    array = filtered.split(" ").map {|word| word.downcase}
+    hash = {}
+    
+    array.uniq.each do |word|
+      hash[word] = array.count(word)
+    end
+    
+    hash
+  end
+  
+  def rhyme_array(rhyme_group)
+    result = ' '
+    
+    if rhyme_group == "0"
+      result = "Could not find any rhymes for that word"
+      
+    else
+      array = Database.find_all_rhymes(rhyme_group)
+      result = array.sample(20).sort{|a, b| a.length <=> b.length}#.join("\n")
+    end
+    result
+  end
+  
+  def random_word()
+    sql = "SELECT rhyme FROM words GROUP BY rhyme ORDER BY rhyme ASC"
+    random_group = Database.query(sql).values.flatten.sample
+    random_rhyme = Database.find_all_rhymes(random_group).sample
+    return random_rhyme
+  end
+  
+  def brain_stormer()
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y"]
+    random_letter = alphabet.shuffle()
+    word = random_word()
+    num = rand()
+    
+    if num > 0.5
+      return word
+    else
+      return random_letter[0]
+    end
+    
+    
+  end
+
+end
---
+++ Words_DB/words_db.rb
@@ -0,0 +1,492 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "pg"
+require 'yaml'
+require 'pry'
+require_relative 'functions'
+
+
+configure do
+  enable :reloader
+  also_reload "/stylesheets/note.css"
+end
+
+
+get "/" do
+  
+  erb :login
+end
+
+post "/login" do
+  username = params[:username]
+  password = params[:password].to_s
+  
+  sql = "SELECT password FROM users WHERE username = $1"
+  correct = Database.execute(sql, username).values.flatten
+  
+  if username && password && correct.empty? != true
+    
+    if password == correct[0]
+      
+      
+      redirect "/home" 
+    end
+  end
+    redirect "/"
+end
+
+
+get "/home" do
+  
+  @result = Database.word_search("ace")
+  
+  @result = "Could Not Find That Word" if @result.empty?
+    
+ erb :home
+end
+
+get "/analyzer" do
+  sql = "SELECT input_str FROM analyzer WHERE id = 1"
+  @input = Database.query(sql).values[0][0]
+  
+  @word_array = analyzer_breakdown(@input)
+  
+  
+  
+erb :analyzer, layout: :layout
+end
+
+post "/analyzer" do
+  input = params[:analyzer_input]
+  sql = "UPDATE analyzer SET input_str = $1 WHERE id = 1"
+  
+  Database.execute(sql, input)
+
+  redirect "/analyzer"
+end
+
+post "/clear_modify" do
+  if params[:clear_modify] == "clear"
+    sql = "DELETE FROM modifier WHERE word IS NOT NULL"
+    Database.delete(sql)
+  end
+  
+  redirect "/word_search"
+end
+
+post "/modify" do
+  
+  modify_word = params[:word]
+  modify_rhyme = params[:rhyme]
+  modify_syllable = params[:syllable]
+  
+  if !Database.word_search(modify_word)
+    Database.add_word(modify_word, modify_rhyme, modify_syllable)
+    Database.delete_modifier(modify_word)
+  else
+    Database.modify_word(modify_word, modify_rhyme, modify_syllable)
+  end
+  
+  
+  
+  redirect "/word_search/#{modify_word}"
+end
+
+get "/word_search" do
+  
+  #display of list to be modified
+  sql = "SELECT * FROM modifier"
+  @modifier_list = Database.query(sql).values
+  @modifier_words = @modifier_list.map do |array|
+    array[0]
+    end
+
+  erb :word_search, layout: :layout  
+end
+
+get "/word_search/:word" do
+  @query_word = params[:word]
+  @result = Database.word_search(@query_word)
+  
+  if @result
+    @rhyme = @result[0][2]
+    @syllable = @result[0][3]
+  else
+    @rhyme = "N/A"
+    @syllable = "N/A"
+  end
+  
+  #display of list to be modified
+  sql = "SELECT * FROM modifier"
+  @modifier_list = Database.query(sql).values
+  @modifier_words = @modifier_list.map do |array|
+    array[0]
+    end
+  
+  erb :word_search, layout: :layout  
+end
+
+post "/word_search" do
+  query_word = params[:word]
+  
+  redirect "/word_search/#{query_word}"
+  
+  erb :word_search, layout: :layout  
+end
+
+
+# displays array of line id and line from note_pad_session table
+# on start up
+
+get "/note_pad" do
+
+  sql = "SELECT * FROM all_lines WHERE saved = 'n'"
+  @current_session = Database.query(sql).values
+  @lines = @current_session.map {|array| [array[0], array[2]] }
+  @syllables = @current_session.map {|array| array[4]}
+  
+  sql = "SELECT * FROM current_session"
+  test = Database.query(sql).values
+  @monkey_test = test[0][1]
+  
+  #find matching rhymes
+  @last_rhyme = "0"
+  @second_last_rhyme = "0"
+  @third_last_rhyme = "0"
+  @last_rhyme_array = []
+  @second_rhyme_array = []
+  @third_rhyme_array = []
+  @syallable_display = Database.find_all_syllables(3)
+  
+  if @lines.length > 0 
+    @last_rhyme = (@current_session[-1][3])
+      if @last_rhyme != "0"
+        @last_rhyme_array = rhyme_array(@last_rhyme).join(" - ")
+      end
+  else
+    @last_rhyme_array = "n/a"
+  end
+  
+  if @lines.length > 1
+    @second_last_rhyme = (@current_session[-2][3])
+    if @second_last_rhyme != "0"
+       @second_rhyme_array = rhyme_array(@second_last_rhyme).join(" - ")
+    end
+  else
+    @second_rhyme_array = "n/a"
+  end
+  
+    if @lines.length > 2
+    @third_last_rhyme = (@current_session[-3][3])
+      if @third_last_rhyme != "0"
+         @third_rhyme_array = rhyme_array(@third_last_rhyme).join(" - ")
+      end
+  else
+    @third_rhyme_array = "n/a"
+  end
+  # end find matcing rhymes
+  
+  @old_lines = ["hey there how are you", "This works?"]
+  
+  erb :note_pad, layout: :layout  
+end
+
+#sumbit will add the line and id to db
+
+post "/note_pad" do
+  #checkboxes for delete lines and save lines
+  if params[:save_all] == "save"
+    Database.save_all()
+    redirect "/note_pad"
+  end
+  
+  if params[:delete_lines] == "Delete"
+    
+    @delete_lines = params[:subject]
+    
+    @delete_lines.each do |line_id|
+      sql = "DELETE FROM all_lines WHERE saved = 'n' AND id = #{line_id}"
+    Database.delete(sql)
+    end
+    redirect "/note_pad"
+  end
+  #---------word search
+  @word_search = params[:word_search]
+  
+  if @word_search != "" || @word_search != nil
+  Database.note_pad_search(@word_search)
+  end
+  
+  #-------------end of word search
+  
+  
+#if clear button is selected, everything is deleted form all_lines where saved is 'n' 
+  clear_session = params[:clear_session]
+  if clear_session == "clear"
+    sql = "DELETE FROM all_lines WHERE saved = 'n'"
+    Database.delete(sql)
+  else  
+  
+  #--------- Analyzes new lines
+    @line = params[:line]
+    
+    if @line == "" || @line == nil
+      redirect "/note_pad"
+    end
+    
+    #gets array of all uniq words in line
+    
+    @pre_analyzed = @line.split(" ").uniq
+
+    #querys all uniq words and creates hash used by analyze method
+    if @pre_analyzed.length > 0
+      @analyzed = {}
+      @pre_analyzed.each do |word|
+       lower_case = word.downcase
+       @analyzed[lower_case] = Database.word_search(lower_case)
+      end
+    #create syallables array and syllable count using @analyzed hash
+      @syllables = analyze(@line, @analyzed).join(" ")
+      @syllable_count = syllable_count(analyze(@line, @analyzed))
+    
+    
+      last_word = (@pre_analyzed[-1])
+      @end_rhyme = 0
+    
+      if @analyzed[last_word] 
+         @end_rhyme = (@analyzed[last_word][0][2])
+      end
+
+      Database.add_line(1, @line, @syllables, @syllable_count, @end_rhyme)
+      # ------- end of analyzing
+      
+      #modifier
+  #use @analyzed hash to find all N/A words (values with false)
+      @unknowns = []
+        @analyzed.each do |word, value|
+          @unknowns << word if value == false
+       end
+      @test = {}
+          if @unknowns.length > 0
+            @unknowns.each do |unknown_word|
+              if !Database.word_search(unknown_word, "modifier")
+                Database.add_to_modifier(unknown_word)
+              end
+            end
+          end
+    #modifier end
+      
+      
+    else
+      redirect "/note_pad"
+    end
+  end  
+  
+  redirect "/note_pad"
+  
+  erb :note_pad, layout: :layout  
+end
+
+
+post "/note_pad/shuffle" do
+    redirect "/note_pad"
+end
+
+get "/all_lines" do
+  sql = "SELECT * FROM all_lines WHERE saved = 'y'"
+  @current_session = Database.query(sql).values
+  
+
+  erb :all_lines, layout: :layout 
+end
+
+get "/all_lines/:sort" do
+  @sort = params[:sort]
+  if @sort == 'new'
+    sql = "SELECT * FROM all_lines WHERE saved = 'y' ORDER BY id DESC"
+    @current_session = Database.query(sql).values
+  else
+    sql = "SELECT * FROM all_lines WHERE saved = 'y' ORDER BY #{@sort}"
+    @current_session = Database.query(sql).values
+  end
+
+  erb :all_lines, layout: :layout 
+end
+
+
+get "/brain_storm" do
+  
+  @random_value = brain_stormer()
+  
+  sql = "SELECT * FROM all_lines WHERE saved = 'n'"
+  @current_session = Database.query(sql).values
+  @lines = @current_session.map {|array| [array[0], array[2]] }
+  @syllables = @current_session.map {|array| array[4]}
+  
+  #find matching rhymes
+  @last_rhyme = "0"
+  @second_last_rhyme = "0"
+  @last_rhyme_array = []
+  @second_rhyme_array = []
+  
+  if @lines.length > 0 
+    @last_rhyme = (@current_session[-1][3])
+    @last_rhyme_array = rhyme_array(@last_rhyme)
+  else
+    @last_rhyme_array = "Could not find any rhymes for that word"
+  end
+  
+  if @lines.length > 1
+    @second_last_rhyme = (@current_session[-2][3])
+   @second_rhyme_array = rhyme_array(@second_last_rhyme)
+  else
+    @second_rhyme_array = "Could not find any rhymes for that word"
+  end
+  # end find matcing rhymes
+  
+  #start of word scramble
+  sql = "SELECT * FROM all_lines WHERE saved = 'y'"
+  current_session = Database.query(sql).values
+  lines = current_session.map{|line| line[2].split(" ")}
+  @fronts = []
+  @backs = []
+  lines.each do |line|
+    random_mid = 0
+    random_mid = Array(0..(line.size - 2)).shuffle[0] if line.size > 2
+  
+    @fronts.push(line.slice!(0..random_mid))
+    @backs.push(line)
+  end
+  
+    @fronts = @fronts.shuffle.map{|line| line.flatten.join(" ")}
+    @backs = @backs.shuffle.map{|line| line.flatten.join(" ")}
+    
+    @shuffled = @fronts.zip(@backs).map{|line| line.join(" ")}
+  
+ 
+  
+  erb :brain_storm, layout: :layout  
+end
+
+#sumbit will add the line and id to db
+
+post "/brain_storm" do
+  
+  #checkboxes for delete lines and save lines
+  if params[:save_all] == "save"
+    Database.save_all()
+    redirect "/brain_storm"
+  end
+  
+  if params[:delete_lines] == "Delete"
+    
+    @delete_lines = params[:subject]
+    
+    @delete_lines.each do |line_id|
+      sql = "DELETE FROM all_lines WHERE saved = 'n' AND id = #{line_id}"
+    Database.delete(sql)
+    end
+    redirect "/brain_storm"
+  end
+  
+  
+#if clear button is selected, everything is deleted form all_lines where saved is 'n' 
+  clear_session = params[:clear_session]
+  if clear_session == "clear"
+    sql = "DELETE FROM all_lines WHERE saved = 'n'"
+    Database.delete(sql)
+  else  
+  
+  
+    @line = params[:line]
+    
+    #gets array of all uniq words in line
+    
+    @pre_analyzed = @line.split(" ").uniq
+
+    #querys all uniq words and creates hash used by analyze method
+    if @pre_analyzed.length > 0
+      @analyzed = {}
+      @pre_analyzed.each do |word|
+       lower_case = word.downcase
+       @analyzed[lower_case] = Database.word_search(lower_case)
+      end
+    #create syallables array and syllable count using @analyzed hash
+      @syllables = analyze(@line, @analyzed).join(" ")
+      @syllable_count = syllable_count(analyze(@line, @analyzed))
+    
+    
+      last_word = (@pre_analyzed[-1])
+      @end_rhyme = 0
+    
+      if @analyzed[last_word] 
+         @end_rhyme = (@analyzed[last_word][0][2])
+      end
+
+      Database.add_line(1, @line, @syllables, @syllable_count, @end_rhyme)
+      
+      
+      #modifier
+  #use @analyzed hash to find all N/A words (values with false)
+      @unknowns = []
+        @analyzed.each do |word, value|
+          @unknowns << word if value == false
+       end
+      @test = {}
+          if @unknowns.length > 0
+            @unknowns.each do |unknown_word|
+              if !Database.word_search(unknown_word, "modifier")
+                Database.add_to_modifier(unknown_word)
+              end
+            end
+          end
+    #modifier end
+      
+      
+    else
+      redirect "/brain_storm"
+    end
+  end  
+  
+  redirect "/brain_storm"
+  
+  erb :brain_storm, layout: :layout  
+end
+
+
+post "/brain_storm/shuffle" do
+    redirect "/brain_storm"
+end
+
+
+get "/random_words" do
+  
+  @random_rhyme = random_word()
+  
+  
+  erb :random_words, layout: :layout 
+end
+
+
+get "/word_scramble" do
+  sql = "SELECT * FROM all_lines WHERE saved = 'y'"
+  @current_session = Database.query(sql).values
+  lines = @current_session.map{|line| line[2].split(" ")}
+  @fronts = []
+  @backs = []
+  lines.each do |line|
+    random_mid = 0
+    random_mid = Array(0..(line.size - 2)).shuffle[0] if line.size > 2
+  
+    @fronts.push(line.slice!(0..random_mid))
+    @backs.push(line)
+  end
+  
+    @fronts = @fronts.shuffle.map{|line| line.flatten.join(" ")}
+    @backs = @backs.shuffle.map{|line| line.flatten.join(" ")}
+    
+    @shuffled = @fronts.zip(@backs).map{|line| line.join(" ")}
+  erb :word_scramble, layout: :layout 
+end
+

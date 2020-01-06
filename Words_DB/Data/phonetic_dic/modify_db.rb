3731164810bcdbfa3f7106fc40c32c85511e6544
Kyle-Schiffli <Kyle.Schiffli@gmail.com>
2020-01-06 17:05:51 -0500

    Create modify_db.rb

---
+++ Words_DB/Data/phonetic_dic/modify_db.rb
@@ -0,0 +1,35 @@
+require 'pg'
+require 'pry'
+
+@words = PG.connect(dbname: "words_app")
+
+# sql = "UPDATE words SET rhyme = $1 WHERE word = $2"
+# @words.exec_params(sql, [rhyme_change, word]) 
+
+sql = "SELECT * FROM words;"
+allwords = @words.exec(sql)
+
+words_hash = {}
+allwords.values.each {|value|
+ words_hash[value[1]] = [value[2], value[3], value[4]]
+}
+
+
+
+loop do
+puts "---- next? ----"
+input = gets.chomp
+
+    if input == "quit"
+        break
+    end
+
+    if input == "search"
+        serach_word = gets.chomp
+        sql = "SELECT * FROM words WHERE word = $1"
+        search_result = @words.exec_params(sql, [serach_word]) 
+        print search_result.values
+
+    end
+
+end

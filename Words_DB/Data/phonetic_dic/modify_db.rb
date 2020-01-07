3ac48faabff93a42e38b832589ce2a774929b62a
Kyle-Schiffli <Kyle.Schiffli@gmail.com>
2020-01-07 16:50:22 -0500

    update db

--- Words_DB/Data/phonetic_dic/modify_db.rb
+++ Words_DB/Data/phonetic_dic/modify_db.rb
@@ -2,44 +2,101 @@ require 'pg'
 require 'pry'
 
 @words = PG.connect(dbname: "words_app")
+sql = "SELECT * FROM words;"
+allwords = @words.exec(sql)
 
 # sql = "UPDATE words SET rhyme = $1 WHERE word = $2"
 # @words.exec_params(sql, [rhyme_change, word]) 
 
-sql = "SELECT * FROM words;"
-allwords = @words.exec(sql)
+sql = "SELECT rhyme FROM words WHERE rhyme != 0;"
+all_rhymes = @words.exec(sql).values
+rhyme_count = all_rhymes.count
+group_count = all_rhymes.uniq.count
+count_by_group = {}
+
+all_rhymes.each {|v| 
+    if count_by_group.has_key?(v)
+        count_by_group[v] += 1
+    else
+        count_by_group[v] = 1
+    end
+}
+
 
 words_hash = {}
 allwords.values.each {|value|
  words_hash[value[1]] = [value[2], value[3], value[4]]
 }
+puts "  "
+puts "  "
+puts "-----------------------------------------------------------------------------------------------"
+puts "  "
+puts "Current Status"
+puts "Total Words: #{allwords.values.count}"
+puts "# of Rhymes: #{rhyme_count}"
+puts "# of Rhyme Groups: #{group_count}"
+puts " "
+puts "-----------------------------------------------------------------------------------------------"
 
 
 
 loop do
 puts "---- Options ----"
-puts "Search"
+puts "Search: s"
+puts "Change"
+puts "Stats"
 puts "Quit: q"
-input = gets.chomp
+input = gets.chomp.downcase
 
     if input == "q"
         break
     end
 
-    if input == "search"
+    if input == "s"
         print "Word: "
         serach_word = gets.chomp
         sql = "SELECT * FROM words WHERE word = $1"
         search_result = @words.exec_params(sql, [serach_word]) 
-        print search_result.values
+        word_result = search_result.values[0][1]
+        rhyme_result = search_result.values[0][2]
+        syl_result = search_result.values[0][3]
+        phone_result = search_result.values[0][4]
+
+        puts "Word: #{word_result}   --  Rhyme: #{rhyme_result}  -- Syl: #{syl_result}  -- Phone: #{phone_result}"
+        puts " "
     end
 
-    if input == "rhyme"
+    if input == "change"
         print "Word: "
-        serach_word = gets.chomp
-        sql = "SELECT * FROM words WHERE word = $1"
-        search_result = @words.exec_params(sql, [serach_word]) 
-        print search_result.values
+        change_word = gets.chomp
+
+        print "Rhyme: "
+        change_rhyme = gets.chomp
+
+        puts "Are you sure?"
+        print "Change #{change_word} to #{change_rhyme}? y/n: "
+        response = gets.chomp
+
+        if response == 'y'
+            sql = "UPDATE words SET rhyme = $1 WHERE word = $2"
+            @words.exec_params(sql, [change_rhyme, change_word]) 
+        end
+
+    end
+
+    if input == "stats"
+        puts "  "
+        puts "  "
+        puts "-----------------------------------------------------------------------------------------------"
+        puts "  "
+        puts "Current Status"
+        puts "Total Words: #{allwords.values.count}"
+        puts "# of Rhymes: #{rhyme_count}"
+        puts "# of Rhyme Groups: #{group_count}"
+        count_by_group.each_key{|k| puts "Group: #{k}    --    #{count_by_group[k]}"}
+        puts " "
+        puts "-----------------------------------------------------------------------------------------------"
+
     end
 
 end
